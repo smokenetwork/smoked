f94ad2a1bf07d4c3bfef41d71b824d835afa2394
Tuan Pham Anh <baabeetaa@gmail.com>
2018-09-23 17:28:39 +0700

    update init recent_claims

--- libraries/chain/database.cpp
+++ libraries/chain/database.cpp
@@ -1995,8 +1995,7 @@ void database::init_genesis( uint64_t init_supply )
              rfo.reward_balance = asset( 0, SMOKE_SYMBOL ); // gpo.total_reward_fund_steem;
 
 #ifndef IS_TEST_NET
-             // (fc::uint128_t(808638359297ull,13744269167557038121ull)); // 14916744862149894120447332012073
-             rfo.recent_claims = (fc::uint128_t(uint64_t(140797515942543623ull)));
+             rfo.recent_claims = (fc::uint128_t(uint64_t(42000000000000ull)));
 #endif
              rfo.author_reward_curve = curve_id::linear; // curve_id::quadratic
              rfo.curation_reward_curve = curve_id::square_root; // curve_id::quadratic_curation
