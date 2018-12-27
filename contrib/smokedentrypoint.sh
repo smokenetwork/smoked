2e586eda97536bc08516a80387da50594999fbfc
Ido Samuelson <ido.samuelson@gmail.com>
2018-12-26 23:59:52 -0700

    Update smokedentrypoint.sh

--- contrib/smokedentrypoint.sh
+++ contrib/smokedentrypoint.sh
@@ -6,7 +6,7 @@ ulimit -c unlimited
 # if we're not using PaaS mode then start smoked traditionally with sv to control it
 if [[ ! "$USE_PAAS" ]]; then
   mkdir -p /etc/service/smoked
-  cp /usr/local/bin/smoke-sv-run.sh /etc/service/smoked/run
+  cp /usr/local/bin/smoked-sv-run.sh /etc/service/smoked/run
   chmod +x /etc/service/smoked/run
   runsv /etc/service/smoked
 else
