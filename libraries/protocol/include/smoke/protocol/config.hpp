cee78fc98d201d976e1caf31e58fdee932bcf2f0
m0ssa99 <lgmosneagu@gmail.com>
2021-05-28 15:04:13 +0000

    Change max witness to 12

--- libraries/protocol/include/smoke/protocol/config.hpp
+++ libraries/protocol/include/smoke/protocol/config.hpp
@@ -55,7 +55,7 @@
 #define SMOKE_BLOCKS_PER_DAY                  (24*60*60/SMOKE_BLOCK_INTERVAL)
 #define SMOKE_START_MINER_VOTING_BLOCK        (1 * 30) // (SMOKE_BLOCKS_PER_DAY * 30)
 
-#define SMOKE_MAX_WITNESSES                   21
+#define SMOKE_MAX_WITNESSES                   12
 
 #define SMOKE_INIT_MINER_NAME                 "initminer"
 
@@ -63,7 +63,7 @@
 #define SMOKE_MAX_MINER_WITNESSES             0
 #define SMOKE_MAX_RUNNER_WITNESSES            1
 
-#define SMOKE_HARDFORK_REQUIRED_WITNESSES     17 // 17 of the 21 dpos witnesses (20 elected and 1 virtual time) required for hardfork. This guarantees 75% participation on all subsequent rounds.
+#define SMOKE_HARDFORK_REQUIRED_WITNESSES     9 // 17 of the 21 dpos witnesses (20 elected and 1 virtual time) required for hardfork. This guarantees 75% participation on all subsequent rounds.
 #define SMOKE_MAX_TIME_UNTIL_EXPIRATION       (60*60) // seconds,  aka: 1 hour
 #define SMOKE_MAX_MEMO_SIZE                   2048
 #define SMOKE_MAX_PROXY_RECURSION_DEPTH       4
